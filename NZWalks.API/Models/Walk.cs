18c20414d01ef4884569655c441845611c317881
PradeepYadav123 <pradeepyadav.101@gmail.com>
2022-11-21 00:08:10 +0530

    End of Secion 2

---
+++ NZWalks.API/Models/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
