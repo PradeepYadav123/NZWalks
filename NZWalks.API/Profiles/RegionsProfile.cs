201a4f90c50382cf3dfc63dd8f15fbbf4cba606b
PradeepYadav123 <pradeepyadav.101@gmail.com>
2022-11-21 23:29:54 +0530

    End of section 3

---
+++ NZWalks.API/Profiles/RegionsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfile: Profile
+    {
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>();
+                //.ForMember(des => des.Id, Options => Options.MapFrom(src=> src.Id)); //for different property name mapping
+               // .ReverseMap();
+
+        }
+    }
+}
